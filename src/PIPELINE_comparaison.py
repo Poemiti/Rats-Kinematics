2aa120ea831b82573f2d828390d95b807c01a50f
Poemiti <poemiti.duprat@gmail.com>
2026-01-30 18:20:39 +0100

    clean

--- src/PIPELINE_comparaison.py
+++ src/PIPELINE_comparaison.py
@@ -8,11 +8,19 @@ import seaborn as sns
 
 from utils.trajectory_metrics import Trajectory
 
-
+METRIC = "toruosity"  # or peak or distance
 THRESHOLD = 0.5
 BODYPART = 'left_hand'
 SINGLE_TRAJ = False
 SHOW = True
+
+
+# plot titles 
+title = "Tortuosity distribution, for CHR rat population\n" \
+        "bodypart observed : left hand, L1 task"
+ylabel = "Tortuosity" # (cm.s$^{-1}$)"
+saving_name = "Violin_tortuosity_CHR_L1.png"
+
 
 # define cm per pixel
 frame_width_m = 0.12 # m
@@ -45,20 +53,21 @@ beta_ON_2_csv = "../data/analysis_results/#517/#517_CHR_Beta_RightHemi_CueL1_H00
 conti_ON_2_csv = "../data/analysis_results/#517/#517_CHR_Conti_RightHemi_CueL1_H001_LaserOn_0,75mW/metrics_per_clips.csv"
 
 # default
-beta_OFF_1_velo = pd.read_csv(beta_OFF_1_csv)['velocity']
-conti_OFF_1_velo = pd.read_csv(conti_OFF_1_csv)['velocity']
+beta_OFF_1_velo = pd.read_csv(beta_OFF_1_csv)[METRIC]
+conti_OFF_1_velo = pd.read_csv(conti_OFF_1_csv)[METRIC]
 
-beta_ON_1_velo = pd.read_csv(beta_ON_1_csv)['velocity']
-conti_ON_1_velo = pd.read_csv(conti_ON_1_csv)['velocity']
+beta_ON_1_velo = pd.read_csv(beta_ON_1_csv)[METRIC]
+conti_ON_1_velo = pd.read_csv(conti_ON_1_csv)[METRIC]
 
 # greater
-beta_OFF_2_velo = pd.read_csv(beta_OFF_2_csv)['velocity']
-conti_OFF_2_velo = pd.read_csv(conti_OFF_2_csv)['velocity']
+beta_OFF_2_velo = pd.read_csv(beta_OFF_2_csv)[METRIC]
+conti_OFF_2_velo = pd.read_csv(conti_OFF_2_csv)[METRIC]
 
-beta_ON_2_velo = pd.read_csv(beta_ON_2_csv)['velocity']
-conti_ON_2_velo = pd.read_csv(conti_ON_2_csv)['velocity']
+beta_ON_2_velo = pd.read_csv(beta_ON_2_csv)[METRIC]
+conti_ON_2_velo = pd.read_csv(conti_ON_2_csv)[METRIC]
 
-
+for data in [beta_OFF_1_velo, beta_OFF_2_velo, conti_OFF_1_velo, conti_OFF_2_velo] : 
+     print(len(data))
 
 def _pack(values, condition, intensity):
         return pd.DataFrame({
@@ -68,33 +77,52 @@ def _pack(values, condition, intensity):
         })
 
 df_long = pd.concat([
-    _pack(beta_OFF_1_velo, "Beta-NoStim", "conti=0.5, beta=1"),
-    _pack(conti_OFF_1_velo, "Conti-NoStim", "conti=0.5, beta=1"),
+    _pack(beta_OFF_1_velo, "NoStim", "conti=0.5, beta=1"),
+    _pack(conti_OFF_1_velo, "NoStim", "conti=0.5, beta=1"),
     _pack(beta_ON_1_velo, "Beta", "conti=0.5, beta=1"),
     _pack(conti_ON_1_velo, "Conti", "conti=0.5, beta=1"),
 
-    _pack(beta_OFF_2_velo, "Beta-NoStim", "conti=0.75, beta=2.5"),
-    _pack(conti_OFF_2_velo, "Conti-NoStim", "conti=0.75, beta=2.5"),
+    _pack(beta_OFF_2_velo, "NoStim", "conti=0.75, beta=2.5"),
+    _pack(conti_OFF_2_velo, "NoStim", "conti=0.75, beta=2.5"),
     _pack(beta_ON_2_velo, "Beta", "conti=0.75, beta=2.5"),
     _pack(conti_ON_2_velo, "Conti", "conti=0.75, beta=2.5"),
 ])
 
+# remove extreme values with the interquarile range method (IQR)
+def trim_extremes_iqr(df, value_col="Value", group_cols=("Condition", "LaserIntensity"), k=1.5):
+    def _trim(group):
+        q1 = group[value_col].quantile(0.25)
+        q3 = group[value_col].quantile(0.75)
+        iqr = q3 - q1
+        low = q1 - k * iqr
+        high = q3 + k * iqr
+        return group[(group[value_col] >= low) & (group[value_col] <= high)]
+
+    return (
+        df
+        .groupby(list(group_cols), group_keys=False)
+        .apply(_trim)
+        .reset_index(drop=True)
+    )
+
+df_long_trimed = trim_extremes_iqr(df_long, k=1.5)
+print(f"\nNumber of removed outliers : {len(df_long) - len(df_long_trimed)}")
+print(df_long_trimed)
+
 # plotting
 fig, ax = plt.subplots()
-title = "Velocity distribution, for CHR rat population\n" \
-        "bodypart observed : left hand, L1 task"
 
-ylabel = "average velocity (m.$s^{-1}$)"
 
 sns.violinplot(
     x="Condition",
     y="Value",
     hue="LaserIntensity",
-    data=df_long,
+    data=df_long_trimed,
     ax=ax,
     split=True,
     gap= .1,
     inner="quart",
+    order=["NoStim", "Conti", "Beta"],
     palette={"conti=0.5, beta=1": "lightblue", "conti=0.75, beta=2.5": "salmon"},
 )
 
@@ -102,7 +130,7 @@ sns.stripplot(
     x="Condition",
     y="Value",
     hue="LaserIntensity",
-    data=df_long,
+    data=df_long_trimed,
     ax=ax,
     dodge=True,
     color="black",
@@ -115,18 +143,18 @@ ax.legend(handles[:2], labels[:2], title="Laser intensity",
           loc='lower right', fontsize=7)
 
 # ---- add counts ----
-counts = df_long.groupby(["Condition", "LaserIntensity"]).size().reset_index(name="N")
-y_max = df_long["Value"].max()
+counts = df_long_trimed.groupby(["Condition", "LaserIntensity"]).size().reset_index(name="N")
+y_max = df_long_trimed["Value"].max()
 y_offset = 0.05 * y_max
 
 for _, row in counts.iterrows():
-    x = ["Conti-NoStim", "Beta-NoStim", "Conti", "Beta"].index(row["Condition"])
+    x = ["NoStim", "Conti", "Beta"].index(row["Condition"])
     x += -0.15 if row["LaserIntensity"] == "conti=0.5, beta=1" else 0.20
 
     ax.text(
         x,
         y_max + y_offset,
-        f"n={row['N']}",
+        f"{row['N']}",
         ha="center",
         va="bottom",
         fontsize=9,
@@ -138,12 +166,13 @@ ax.set_ylabel(ylabel)
 ax.set_title(title)
 ax.grid(axis="y", alpha=0.3)
 
-plt.savefig(OUTPUT_DIR / "Violin_mean_velocity_CHR_L1.png")
+plt.savefig(OUTPUT_DIR / saving_name)
 
 if SHOW : 
     plt.show()
 
 plt.close(fig)
+
 
 
 # =================================================================== CTRL CONDITION
@@ -153,9 +182,9 @@ plt.close(fig)
 # beta_ON_1_csv = "../data/analysis_results/#517/#517_CTRL_Beta_LeftHemi_CueL1_H001_LaserOn/metrics_per_clips.csv"
 # conti_ON_1_csv = "../data/analysis_results/#517/#517_CTRL_Conti_LeftHemi_CueL1_H001_LaserOn/metrics_per_clips.csv"
 
-# noStim_velo = pd.read_csv(noStim_csv)['velocity']
-# beta1_velo = pd.read_csv(beta_ON_1_csv)['velocity']
-# conti1_velo = pd.read_csv(conti_ON_1_csv)['velocity']
+# noStim_velo = pd.read_csv(noStim_csv)[METRIC]
+# beta1_velo = pd.read_csv(beta_ON_1_csv)[METRIC]
+# conti1_velo = pd.read_csv(conti_ON_1_csv)[METRIC]
 
 
 # fig, ax = plt.subplots()
