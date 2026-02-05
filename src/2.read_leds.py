d3ddc34ed5e262b14d629570c9992238a8fc2c01
Poemiti <poemiti.duprat@gmail.com>
2026-02-05 14:12:44 +0100

    clean

--- src/2.read_leds.py
+++ src/2.read_leds.py
@@ -2,67 +2,34 @@
 
 from pathlib import Path
 import pandas as pd
-import matplotlib.pyplot as plt
-import os
 
-from utils.file_management import make_database, is_csv, is_video, is_left_view, verify_exist
-from utils.database_filter import Model, Controller, View
+from utils.file_management import is_left_view
 from utils.led_detection import get_luminosity, rename_file, define_cue_type, is_led_on
+from config import load_config
+from pipeline_maker import load_database
 
 
+# ------------------------------------ setup ---------------------------------------
 
-# ------------------------------------ setup path ---------------------------------------
-
-GENERATED_DATA_DIR = Path("../data")
-CLIP_DIR = GENERATED_DATA_DIR / "clips"
-TRAJECTORY_DIR = GENERATED_DATA_DIR / "csv_results"
-DATABASE_DIR = GENERATED_DATA_DIR / "database" 
-
-# ------------------------------------ make database out of the clip directory ---------------------------------------
-
-raw_database = make_database(CLIP_DIR, is_video)
-
-model = Model(raw_database, DATABASE_DIR)
-view = View()
-controller = Controller(model, view)
-view.mainloop()
-
-DATABASE = controller.filtered_dataset.reset_index(drop=True)
-print(DATABASE)
-
-# save database
-if controller.dataset_name.get() :
-    dataset_name = f"{controller.dataset_name.get().strip()}.csv"
-    DATABASE.to_csv(DATABASE_DIR / dataset_name)
-    print(f"\nFiltered dataset saved as : {DATABASE_DIR / dataset_name}")
-
-print(f"\nNumber of files in database : {len(DATABASE)}")
+cfg = load_config()
+DATABASE = load_database()
 
 RAT_NAME = DATABASE['rat_name'][0]
 
-LUMINOSITY_DIR = GENERATED_DATA_DIR / "luminosity" / RAT_NAME
-LUMINOSITY_DIR.mkdir(parents=True, exist_ok=True)
-
-
 # ------------------------------------ get luminosity + classify ---------------------------------------
 
-count = 0
 
 for i, video_path in enumerate(DATABASE["filename"].iloc[:]) : 
-
-
     video_path = Path(video_path)
 
-    # verify_exist(video_path)
-
     print(f"\n[{i}/{len(DATABASE)}]")
-    print(f"Getting luminosity of {video_path}")
+    print(f"Getting luminosity of {video_path}\n")
 
     if "LaserOff" in video_path.stem or "LaserOn" in video_path.stem:
         print("already done!")
         continue
 
-    output_dir = LUMINOSITY_DIR / video_path.parent.stem  # get the folder name
+    output_dir = cfg.paths.luminosity / RAT_NAME / video_path.parent.stem  # get the folder name
     output_dir.mkdir(parents=True, exist_ok=True)
 
     html_output_path = output_dir / f"luminosity_{video_path.stem}.html"
@@ -89,11 +56,9 @@ for i, video_path in enumerate(DATABASE["filename"].iloc[:]) :
     luminosities.columns = luminosities.columns.droplevel(0) # columns = LED_1 ...
     luminosities = luminosities.drop([1]).reset_index(drop=True)  # remove useless row
 
-    print(luminosities)
-
     # get the trajectory prediction csv path for renaming
     clip_number = str(video_path.stem)[-8:]
-    trajectory_csv_dir = TRAJECTORY_DIR / str(video_path.stem)[:-8]
+    trajectory_csv_dir = cfg.paths.coords / RAT_NAME / str(video_path.stem)[:-8]
     trajectory_csv_path = trajectory_csv_dir / f"pred_results_{video_path.stem}.csv"
     
     # if not verify_exist(trajectory_csv_path) : 
@@ -111,6 +76,4 @@ for i, video_path in enumerate(DATABASE["filename"].iloc[:]) :
                     laser_on=led_on,
                     new_cue=cue_type)
         
-    count += 1
-
 print("Done !")
